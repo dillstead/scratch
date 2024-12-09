0633c78b7fcbbb1971407edfde62ca3b5e8f9e88
Steadman Dillroper <dillstead@gmail.com>
2024-12-09 17:09:55 -0500

    AOC Problem 9A 2024 Update

---
+++ aoc/2024/problem9a.py
@@ -0,0 +1,28 @@
+import sys
+
+disk = []
+for id, bl in enumerate(sys.stdin.read().strip()):
+    disk.extend(int(bl) * ([-1] if id % 2 == 1 else [int(id / 2)]))
+i, j = 0, len(disk) - 1
+while i < j:
+    if disk[i] == -1 and disk[j] == -1:
+        j -= 1
+    elif disk[i] != -1 and disk[j] != -1:
+        i += 1
+    elif disk[i] == -1 and disk[j] != -1:
+        disk[i] = disk[j]
+        disk[j] = -1
+        i += 1
+        j -= 1
+    else: 
+        i += 1
+print(disk)        
+sum = 0
+for i in range(j + 1):
+    sum += (i * disk[i])
+print(sum)    
+
+
+
+
+
