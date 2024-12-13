a3645a807b3018a666fbfe37e1bdf627b9dbfb1f
Steadman Dillroper <dillstead@gmail.com>
2024-12-13 11:56:34 -0500

    AOC Problem 9A 2024 Update

--- aoc/2024/problem9a.py
+++ aoc/2024/problem9a.py
@@ -5,17 +5,13 @@ for id, bl in enumerate(sys.stdin.read().strip()):
     disk.extend(int(bl) * ([-1] if id % 2 == 1 else [int(id / 2)]))
 i, j = 0, len(disk) - 1
 while i < j:
-    if disk[i] == -1 and disk[j] == -1:
-        j -= 1
-    elif disk[i] != -1 and disk[j] != -1:
-        i += 1
-    elif disk[i] == -1 and disk[j] != -1:
+    t = disk[i]
+    if disk[i] == -1 and disk[j] != -1:
         disk[i] = disk[j]
-        disk[j] = -1
+    if t != -1 or disk[j] != -1:
         i += 1
-        j -= 1
-    else: 
-        i += 1
+    if t == -1:
+        j -= 1	
 sum = 0
 for i in range(j + 1):
     sum += (i * disk[i])
