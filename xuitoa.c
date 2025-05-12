8e87cd55c21ad018fede95747b68e27686790d92
Steadman Dillroper <dillstead@gmail.com>
2025-05-12 19:04:25 +0100

    hex uitoa

---
+++ xuitoa.c
@@ -0,0 +1,32 @@
+// gcc -Werror -std=c99 -Wall -Wextra -O2 -o xuitoa xuitoa.c
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define duitoa(i,s) uitoa((i),(s),10,"0123456789")
+#define xuitoa(i,s) uitoa((i),(s),16,"0123456789abcdef")
+
+static char *uitoa(unsigned int i, char *s, int base, const char *digits)
+{
+    int len = 1;
+    int t = i;
+    while (i /= base) len++;
+    char *beg = s + len;
+    *beg = '\0';
+    do
+    {
+        *--beg = digits[t % base];
+    } while (t /= base);
+    return s;
+}
+
+int main(void)
+{
+    char buf[64];
+    assert(!strcmp(duitoa(0, buf), "0"));
+    assert(!strcmp(duitoa(87658, buf), "87658"));
+    assert(!strcmp(duitoa(2147483647, buf), "2147483647"));
+    assert(!strcmp(xuitoa(87658, buf), "1566a"));
+    assert(!strcmp(xuitoa(2147483647, buf), "7fffffff"));
+    return EXIT_SUCCESS;
+}
