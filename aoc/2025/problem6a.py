9f7dfcdbe95105cbd8189fe3e7f394c0ec8f8fd4
Steadman Dillroper <dillstead@gmail.com>
2025-12-08 11:48:30 -0500

    Update Problem 6A

--- aoc/2025/problem6a.py
+++ aoc/2025/problem6a.py
@@ -4,7 +4,6 @@ from operator import mul
 
 for line in fileinput.input():
     toks = line.split()
-#    print(toks)
     if fileinput.isfirstline():
         prod = list(map(int, toks))
         sm = prod.copy()
